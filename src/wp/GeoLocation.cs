ce26ed15eb06bf3611af324111502c2d30b7c4d0
Viras- <viras@users.sourceforge.net>
2013-08-15 11:07:07 +0200

    fixing number formatting for different culture versions adding comments to code

--- src/wp/GeoLocation.cs
+++ src/wp/GeoLocation.cs
@@ -32,6 +32,9 @@ namespace WPCordovaClassLib.Cordova.Commands
         protected Dictionary<string, string> watchIds;
         protected static int watcher_timeout = 5000;
 
+        /// <summary>
+        /// Prepare GeoLocation watching
+        /// </summary>
         public Geolocation()
         {
             watchIds = new Dictionary<string, string>();
@@ -41,11 +44,17 @@ namespace WPCordovaClassLib.Cordova.Commands
             watcher.StatusChanged += new EventHandler<GeoPositionStatusChangedEventArgs>(watcher_PositionStatusChanged);
         }
 
+        /// <summary>
+        /// Start watching the location
+        /// </summary>
         protected void startWatcher()
         {
             watcher.TryStart(true, TimeSpan.FromMilliseconds(watcher_timeout));
         }
 
+        /// <summary>
+        /// Stop watching the location
+        /// </summary>
         protected void stopWatcher()
         {
             if (watchIds.Count <= 0)
@@ -55,38 +64,54 @@ namespace WPCordovaClassLib.Cordova.Commands
         }
 
         /// <summary>
-        /// Helper function for forcing a valid number and preventing NaN as result
+        /// Helper function for forcing a valid number and preventing NaN as result, in addition it takes care of correct number formatting
         /// </summary>
         /// <param name="input">value to check</param>
-        /// <returns>double value, but NaN as 0.0</returns>
-        protected double forceNumber(double input)
+        /// <returns>string value, but NaN as 0.0</returns>
+        protected string formatNumber(double input)
         {
+            string value = "";
+
+            // we do not want NaN as a result
             if (double.IsNaN(input))
             {
-                return 0.0;
+                value = "0.0";
             }
             else
             {
-                return input;
+                value = input.ToString();
             }
+
+            // make sure we use a '.' as separator
+            return value.Replace(',', '.');
         }
 
+
+        /// <summary>
+        /// Format a GeoCoordinate object into a JSON object for sending to the brower
+        /// </summary>
+        /// <param name="coord">Coordinate object to convert</param>
+        /// <returns>string containing a JSON-formatted object representation</returns>
         protected string getLocationJSON(GeoCoordinate coord)
         {
             string res = String.Format("\"latitude\":{0},\"longitude\":{1},\"altitude\":{2},\"accuracy\":{3},\"heading\":{4},\"velocity\":{5},\"altitudeAccuracy\":{6}",
-                forceNumber(coord.Latitude),
-                forceNumber(coord.Longitude),
-                forceNumber(coord.Altitude),
-                forceNumber(coord.HorizontalAccuracy),
-                forceNumber(coord.Course),
-                forceNumber(coord.Speed),
-                forceNumber(coord.VerticalAccuracy)
+                formatNumber(coord.Latitude),
+                formatNumber(coord.Longitude),
+                formatNumber(coord.Altitude),
+                formatNumber(coord.HorizontalAccuracy),
+                formatNumber(coord.Course),
+                formatNumber(coord.Speed),
+                formatNumber(coord.VerticalAccuracy)
                 );
             res = "{" + res + "}";
 
             return res;
         }
 
+        /// <summary>
+        /// Called from the JavaScript bridge when the location should be fetched
+        /// </summary>
+        /// <param name="options">JSON-parameters</param>
         public void getLocation(string options)
         {
             startWatcher();
@@ -107,6 +132,10 @@ namespace WPCordovaClassLib.Cordova.Commands
             DispatchCommandResult(pluginResult);
         }
 
+        /// <summary>
+        /// Called from the JavaScript bridge when location changes should be watched
+        /// </summary>
+        /// <param name="options"></param>
         public void addWatch(string options)
         {
             string watchId = JsonHelper.Deserialize<string[]>(options)[0];
@@ -115,12 +144,12 @@ namespace WPCordovaClassLib.Cordova.Commands
             watchIds.Add(watchId, callbackId);
 
             startWatcher();
-
-            /*PluginResult pluginResult = new PluginResult(PluginResult.Status.OK);
-            pluginResult.KeepCallback = true;
-            DispatchCommandResult(pluginResult);*/
         }
 
+        /// <summary>
+        /// Stop watching the location
+        /// </summary>
+        /// <param name="options"></param>
         public void clearWatch(string options)
         {
             string watchId = JsonHelper.Deserialize<string[]>(options)[0];
@@ -132,6 +161,11 @@ namespace WPCordovaClassLib.Cordova.Commands
             DispatchCommandResult();
         }
 
+        /// <summary>
+        /// Callback which is called from the GeoCoordinateWatcher class once the location has changed
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         public void watcher_PositionChanged(object sender, GeoPositionChangedEventArgs<GeoCoordinate> e)
         {
             string res = getLocationJSON(e.Position.Location);
@@ -144,6 +178,11 @@ namespace WPCordovaClassLib.Cordova.Commands
             }
         }
 
+        /// <summary>
+        /// Called when the status of the position tracking has changed
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         public void watcher_PositionStatusChanged(object sender, GeoPositionStatusChangedEventArgs e)
         {
         }
